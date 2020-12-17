f6a51c18e9dc9c785eccf0b32e464ebaffe1e9b6
Peter <ptrwlr@gmail.com>
2020-12-16 21:28:33 -0600

    added prefix command to database

---
+++ src/commands/moderation/prefix.js
@@ -0,0 +1,26 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const GuildConfig = require('../../database/schemas/guildconfig');
+
+module.exports = class PrefixCommand extends BaseCommand {
+  constructor() {
+    super('prefix', 'moderation', []);
+  }
+  
+  async run(client, message, args) {
+    if (message.type === 'DM' || (!message.member.hasPermission('ADMINISTRATOR'))) return;
+    const guildId = client.guilds.cache.get(message.guild.id).id;
+    const parseArgs = args.slice(1).toLowerCase().trim().split('-');
+    const prefixUpdate = parseArgs[0];
+    if ((prefixUpdate.length <= 3) && (prefixUpdate.length > 0)) {
+        const prefixData = await GuildConfig.findOneAndUpdate(message.guild.id, {prefix: prefixUpdate}, {new: true});
+        if (prefixData) {
+            message.reply(`The new Prefix is \`${prefixUpdate}\``);
+        } else (err) => {
+            message.reply('There was an issue updating the prefix');
+            console.log(err);
+        }
+    } else {
+        message.reply('The prefix needs to be 1-3 characters long')
+    }
+  }
+}
