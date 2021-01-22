e0a1db2ad2fd7bdc7db2d3d9f2ffc28d354e5705
Peter <ptrwlr@gmail.com>
2021-01-21 19:21:46 -0600

    fixed stupid typo in CC command

--- src/commands/moderation/clearchannel.js
+++ src/commands/moderation/clearchannel.js
@@ -9,7 +9,7 @@ module.exports = class ClearChannelCommand extends BaseCommand {
       if (message.member.hasPermission('ADMINISTRATOR')) {
           message.channel.messages.fetch().then((results) => {
               message.channel.bulkDelete(results, true);
-              message.channel.send('I deleted all of the messages that I could. (I cannot delete messages over 14 days old');
+              message.channel.send('I deleted all of the messages that I could. (I cannot delete messages over 14 days old)');
           })
       }
   }
