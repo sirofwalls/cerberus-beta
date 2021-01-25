be2db963b316ab9d056b1ba124ff901f6d726984
Peter <ptrwlr@gmail.com>
2021-01-25 12:33:47 -0600

    fixed formating in help command

--- src/commands/help/help.js
+++ src/commands/help/help.js
@@ -6,27 +6,26 @@ module.exports = class HelpCommand extends BaseCommand {
     super('help', 'help', [], 'It is a help command... it shows the commands.');
   }
 
-  async run(client, message, args) {    
-    const embed = new Discord.MessageEmbed()
+  async run(client, message, args) {
+      const embed = new Discord.MessageEmbed()
       .setColor('GREEN')
       .setTitle('Commands List')
       .addFields([])
+      
+      let commands = client.commands;
   
-  let commands = client.commands;
-  
-  const arrayCheck = [];
-  commands.forEach(command => {
-      if(command.category === 'admin' || command.category === 'owner') return;
-    if(arrayCheck.includes(command.name)) return;
-    arrayCheck.push(command.name);
+        const arrayCheck = [];
+         commands.forEach(command => {
+             if(command.category === 'admin' || command.category === 'owner') return;
+             if(arrayCheck.includes(command.name)) return;
+            arrayCheck.push(command.name);
 
-    embed.fields.push({
-        name: `${command.name.toUpperCase()}`,
-        value: `**Aliases:** ${command.aliases}\n**Category:** ${command.category.toUpperCase()}\n**Description:** ${command.description}\n----------`
-    });
-  });
-
-  message.channel.send(embed);
-
-  }
+            embed.fields.push({
+                name: `${command.name.toUpperCase()}`,
+                value: `**Aliases:** ${command.aliases}\n**Category:** ${command.category.toUpperCase()}\n**Description:** ${command.description}\n----------`
+            });
+        });
+        
+        message.channel.send(embed);
+    }
 }
